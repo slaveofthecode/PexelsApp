6c9f2c83d4b9c6dd03f4485dc270d23fcfc3d750
Gustavo Lopez <gustavo@ohana.digital>
2021-09-19 01:32:33 -0300

    styles on the ImageScreen screens

--- screens/ImageScreen.js
+++ screens/ImageScreen.js
@@ -1,18 +1,44 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import {View, Image, StyleSheet} from 'react-native';
+import {View, Text, Image, StyleSheet} from 'react-native';
+import {Card, Button, Icon} from 'react-native-elements';
 
 const ImageScreen = ({route}) => {
   const {image} = route.params;
 
   return (
-    <View>
-      <Image
-        source={{
-          uri: image.src.medium,
-          height: '75%',
-          width: '100%',
-        }}
-      />
+    <View style={{backgroundColor: '#000', flex: 1}}>
+      <Card containerStyle={style.containerCard}>
+        <Card.Title style={{color: '#fff', textTransform: 'capitalize'}}>
+          {image.photographer}
+        </Card.Title>
+        <View>
+          <Image
+            source={{
+              uri: image.src.medium,
+              height: 350,
+            }}
+          />
+          <Text
+            style={{
+              position: 'absolute',
+              bottom: 0,
+              height: 50,
+              width: '100%',
+              backgroundColor: 'rgba(0,0,0,.75)',
+              color: '#fff',
+              padding: 15,
+            }}>
+            {image.id}
+          </Text>
+        </View>
+        <Card.Divider width={1} style={{marginVertical: 15}} />
+        <Button
+          icon={<Icon name="download" type="font-awesome" color="#ffffff" />}
+          title="download"
+          buttonStyle={{backgroundColor: 'rgb(5, 160, 129)'}}
+        />
+      </Card>
     </View>
   );
 };
@@ -20,8 +46,9 @@ const ImageScreen = ({route}) => {
 export default ImageScreen;
 
 const style = StyleSheet.create({
-  image: {
-    height: 150,
-    width: 150,
+  containerCard: {
+    height: '82%',
+    backgroundColor: '#2C292C',
+    borderWidth: 0,
   },
 });
